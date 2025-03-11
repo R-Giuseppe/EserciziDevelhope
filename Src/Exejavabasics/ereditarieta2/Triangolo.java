e478c1ace41cc9d322590cb2dd99e43b163c090b
R-Giuseppe <peppe.rosace@gmail.com>
2025-03-11 22:03:48 +0100

    exe polimorfismo

---
+++ Src/Exejavabasics/ereditarieta2/Triangolo.java
@@ -0,0 +1,18 @@
+package Exejavabasics.ereditarieta2;
+
+public class Triangolo extends Forma{
+    private int base;
+    private int altezza;
+
+    public Triangolo(int base, int altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    @Override
+    public void calcolaArea() {
+        int area = (base * altezza) / 2;
+        System.out.println("L'area del triangolo è: " + area);
+    }
+
+}
