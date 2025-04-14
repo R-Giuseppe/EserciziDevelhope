873b51c686267a7fc64741ab07f23f493d3a9c00
R-Giuseppe <peppe.rosace@gmail.com>
2025-04-14 15:06:52 +0200

    exe result

--- Src/Exejavabasics/hashset2/MainFruit.java
+++ Src/Exejavabasics/hashset2/MainFruit.java
@@ -7,9 +7,20 @@ public class MainFruit {
         hashsetFruit.add("Banana");
         hashsetFruit.add("Pera");
         hashsetFruit.add("Arancia");
+        String frutto = "Ananas";
 
         for (String fruit : hashsetFruit.getStringHashSet()){
             System.out.println(fruit);
         }
+        for (String elementoCorrenteHashset : hashsetFruit.getStringHashSet()){
+            if (elementoCorrenteHashset.equals(frutto)){
+                System.out.println("Trovato");
+                break;
+            } else {
+                System.out.println("Non trovato");
+                break;
+            }
+        }
+
     }
 }
